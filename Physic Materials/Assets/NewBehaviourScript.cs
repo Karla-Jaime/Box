c9b1fd3be6c12ed4dc32dc70efdd241d414f7325
Karly-98 <Karly-98@DESKTOP-D2ALSSH>
2019-02-24 13:51:07 -0700

    caja
    box

---
+++ Physic Materials/Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+          //vector up, 1x y 0y
+        rb2d.AddForce(Vector2.up * 1000, ForceMode2D.Force);
+    }
+}
+
+
